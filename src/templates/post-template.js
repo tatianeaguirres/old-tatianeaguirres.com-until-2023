38ecd6f9d50c384a9afba65028f1fbb639e9d550
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-07-28 15:14:24 +0200

    :sparkles: add comments with Disqus

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,18 +1,42 @@
 import React from 'react'
 import { PageLayout, PageTitle } from '../components'
 import SEO from '../utils/seo'
+import { DiscussionEmbed } from 'disqus-react'
 
-export default ({ title, description, html, subTitle, image, pathname }) => (
-  <PageLayout>
-    <SEO
-      title={title}
-      description={description}
-      image={image}
-      pathname={pathname}
-    />
-    <PageTitle title={title} />
-    {subTitle}
-    <div dangerouslySetInnerHTML={{ __html: html }} />
-    <p className='u-logo u-margin-bottom-xs'>Tati</p>
-  </PageLayout>
-)
+const PostTemplate = ({
+  title,
+  description,
+  html,
+  subTitle,
+  image,
+  pathname
+}) => {
+  const shortname = 'tatianeaguirres'
+  const url = 'https://www.tatianeaguirres.com' + pathname
+  const threadConfig = {
+    url: url,
+    identifier: url,
+    title: title
+  }
+  return (
+    <PageLayout>
+      <SEO
+        title={title}
+        description={description}
+        image={image}
+        pathname={pathname}
+      />
+      <PageTitle title={title} />
+      {subTitle}
+      <div dangerouslySetInnerHTML={{ __html: html }} />
+      <p className='u-logo u-margin-bottom-xlg'>Tati</p>
+      <DiscussionEmbed
+        className='disqus-box'
+        shortname={shortname}
+        config={threadConfig}
+      />
+    </PageLayout>
+  )
+}
+
+export default PostTemplate
