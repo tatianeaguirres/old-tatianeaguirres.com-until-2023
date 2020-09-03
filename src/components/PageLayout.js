a89f127ff03d4b584973b4f84e59ae90e20ca79a
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-09-03 10:26:19 +0200

    :fire: Remove background images

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -2,15 +2,11 @@ import React from 'react'
 import Header from './Header'
 import Container from './Container/Container'
 import Footer from './Footer/Footer'
-import TopImage from './TopImage'
-import BottomImage from './BottomImage'
 
 export default ({ children }) => (
   <>
-    <TopImage />
     <Header />
     <Container>{children}</Container>
     <Footer />
-    <BottomImage />
   </>
 )
