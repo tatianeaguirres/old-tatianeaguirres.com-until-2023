fbd923aae310716816d935a25d8071304cc3142e
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-09-06 18:59:25 +0200

    :art: update blog link component

--- src/components/BlogLink/BlogLink.js
+++ src/components/BlogLink/BlogLink.js
@@ -1,22 +1,19 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import Img from 'gatsby-image'
 import './BlogLink.scss'
-import LabelIcon from '../../../assets/svg/label.svg'
 
 export default props => {
   return (
     <Link className='blog-link' to={props.to} title={props.title}>
-      <article className='blog-link__post'>
+      <article className='blog-link__post u-text-center'>
+        <Img
+          fluid={props.featuredImage}
+          objectFit='cover'
+          objectPosition='50% 50%'
+        />
         <h3>{props.title}</h3>
-        <time>{props.date}</time>
-        <p>{props.description}</p>
         <small className='blog-link__tags'>
-          <img
-            src={LabelIcon}
-            className='blog-link__icon'
-            alt='label'
-            title='label'
-          />
           {props.tags.map((tag, index) => (
             <span key={index}>{tag}</span>
           ))}
