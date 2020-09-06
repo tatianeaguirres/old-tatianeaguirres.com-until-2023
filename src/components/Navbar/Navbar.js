729aca917e93383c5579a37345270bac92bcd871
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-09-06 18:55:40 +0200

    :art: update navbar component

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,51 +1,93 @@
-import React, { useContext } from 'react'
-import ThemeContext from '../../utils/theme'
-import { Navbar, Nav, Form } from 'react-bootstrap'
+import React, { forwardRef } from 'react'
 import { Link } from 'gatsby'
+import useLockBodyScroll from '../../hooks/use-lock-body-scroll'
 import './Navbar.scss'
+import CloseIcon from '../../../assets/svg/close.svg'
 
-export default () => {
-  const { dark, toggleDark, toString } = useContext(ThemeContext)
+const Navbar = forwardRef((props, ref) => {
+  const device = props.props.device
+  const closeMenu = props.props.closeMenu
+
+  if (device === 'mobile') useLockBodyScroll()
+
   return (
-    <Navbar variant={toString()} fixed='top' collapseOnSelect expand='md'>
-      <Navbar.Brand as={Link} to='/'>
-        <h1>
-          Tatiane <span className='u-desktop-only'>Aguirres Nogueira</span>
-        </h1>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls='responsive-navbar-nav' />
-      <Navbar.Collapse
-        id='responsive-navbar-nav'
-        className='justify-content-end'
-      >
-        <Nav className='nav-links'>
-          <Nav.Link as={Link} to='/' title='Home'>
-            Home
-          </Nav.Link>
-          <Nav.Link as={Link} to='/blog' title='Blog'>
-            Blog
-          </Nav.Link>
-          <Nav.Link as={Link} to='/about' title='About'>
-            About
-          </Nav.Link>
-          <Nav.Link as={Link} to='/ebook' title='eBook'>
-            eBook
-          </Nav.Link>
-          <Nav.Link as={Link} to='/newsletter' title='Newsletter'>
-            Newsletter
-          </Nav.Link>
-          <Form className='my-auto'>
-            <Form.Check
-              type='switch'
-              id='custom-switch'
-              label=''
-              title='Toggle Theme'
-              checked={dark}
-              onChange={toggleDark}
-            />
-          </Form>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div
+      ref={ref}
+      className={device === 'mobile' ? 'c-navbar--mobile' : 'c-navbar--desktop'}
+    >
+      <nav aria-labelledby='mainmenulabel' className='navbar__nav'>
+        <h2 id='mainmenulabel' className='u-sr-only'>
+          Main Menu
+        </h2>
+
+        {device === 'mobile' && (
+          <div className='navbar__button-row'>
+            <button
+              onClick={closeMenu}
+              aria-label='Close menu'
+              className='navbar__close-button'
+            >
+              <span className='u-sr-only'>Clique here to close the menu</span>
+              <img src={CloseIcon} className='navbar__close-icon' />
+            </button>
+          </div>
+        )}
+
+        <ul className='navbar__nav-list'>
+          <li className='navbar__nav-item'>
+            <Link
+              to='/'
+              activeClassName='active'
+              className='navbar__nav-link'
+              onClick={closeMenu}
+            >
+              Home
+            </Link>
+          </li>
+          <li className='navbar__nav-item'>
+            <Link
+              to='/blog'
+              activeClassName='active'
+              className='navbar__nav-link'
+              onClick={closeMenu}
+            >
+              Blog
+            </Link>
+          </li>
+          <li className='navbar__nav-item'>
+            <Link
+              to='/about'
+              activeClassName='active'
+              className='navbar__nav-link'
+              onClick={closeMenu}
+            >
+              About
+            </Link>
+          </li>
+          <li className='navbar__nav-item'>
+            <Link
+              to='/ebook'
+              activeClassName='active'
+              className='navbar__nav-link'
+              onClick={closeMenu}
+            >
+              eBook
+            </Link>
+          </li>
+          <li className='navbar__nav-item'>
+            <Link
+              to='/newsletter'
+              activeClassName='active'
+              className='navbar__nav-link'
+              onClick={closeMenu}
+            >
+              Newsletter
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
   )
-}
+})
+
+export default Navbar
