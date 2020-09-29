c910be9374c796fc14ba16a51b597af47c50cf8d
Tatiane Aguirres Nogueira <aguirres.tati@gmail.com>
2020-09-29 14:26:20 +0200

    :art: improve structure of the code

--- src/components/PageLayout.js
+++ src/components/PageLayout.js
@@ -1,32 +1,23 @@
-import React, { useState } from 'react'
+import React from 'react'
 import CookieConsent from 'react-cookie-consent'
 import { Link } from 'gatsby'
 import Header from './Header/Header'
 import Container from './Container/Container'
 import Footer from './Footer/Footer'
-import Modal from './Modal/Modal'
 
 const PageLayout = ({ children, page }) => {
-  const [openModal, setOpenModal] = useState(false)
-
   return (
     <>
       <Header />
       <Container page={page}>{children}</Container>
       <Footer />
       <CookieConsent
-        buttonText='Accept'
+        buttonText='OK'
         disableStyles={true}
         buttonClasses='u-button'
         containerClasses='cookie-consent__container'
         contentClasses='cookie-consent__content'
         cookieName='tatianeaguirres-google-analytics'
-        acceptOnScroll={true}
-        onAccept={({ acceptedByScrolling }) => {
-          if (acceptedByScrolling) {
-            setOpenModal(true)
-          }
-        }}
       >
         This website uses cookies to do static analyses and enhance the user
         experience. By using our website, you hereby consent to our{' '}
@@ -35,7 +26,6 @@ const PageLayout = ({ children, page }) => {
         </Link>{' '}
         and agree to its terms.
       </CookieConsent>
-      {openModal && <Modal onClose={() => setOpenModal(false)} />}
     </>
   )
 }
